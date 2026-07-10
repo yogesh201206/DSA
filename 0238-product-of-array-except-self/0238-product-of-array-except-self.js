00825c134f332d3c8d16ed07fabbc4b1f40aa063
Yogesh M P <yogesgamer35@gmail.com>
2026-07-10 19:17:33 +0530

    Time: 4 ms (86.56%), Space: 69.3 MB (79.87%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const n = nums.length;
+    const ans = new Array(n);
+
+    for(let i = 0,left = 1; i < n; i++){
+        ans[i] = left;
+        left *= nums[i];
+    }
+
+    for(i = n - 1, right = 1; i >=0; i-- ){
+        ans[i] *= right;
+        right *= nums[i];
+    }
+
+    return ans;
+};
