bc8fc4be128f3fbea7b40719bb609d7e6578fcd4
Yogesh M P <yogesgamer35@gmail.com>
2026-07-11 19:13:57 +0530

    Time: 0 ms (100%), Space: 53.7 MB (66.07%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let i = -1;
+    let j = nums.length;
+    let k = 0;
+    while (k < j) {
+        if (nums[k] === 0) {
+            ++i;
+            [nums[i], nums[k]] = [nums[k], nums[i]];
+            ++k;
+        } else if (nums[k] === 2) {
+            --j;
+            [nums[j], nums[k]] = [nums[k], nums[j]];
+        } else {
+            ++k;
+        }
+    }
+};
