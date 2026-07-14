297511eb2dbc51df792407d27ed38428cc62e126
Yogesh M P <yogesgamer35@gmail.com>
2026-07-14 19:43:42 +0530

    Time: 4 ms (14.92%), Space: 63.3 MB (42.66%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,18 @@
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
+
+    while (left < right) {
+        const width = right - left;
+        const currentArea = Math.min(height[left], height[right]) * width;
+        maxArea = Math.max(maxArea, currentArea);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return maxArea;
+};
